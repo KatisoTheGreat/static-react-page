1a62a38f53d01acfa7d836c57a18d3550cad57a7
KatisoTheGreat <katisoledwaba@gmail.com>
2023-05-31 21:40:40 +0200

    updated file structure

--- Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Footer() {
-    return (
-        <footer className="footer"><small>© 2023 KatisoTheGreat development. All rights reserved.</small></footer>
-    )
-}
-
-export default Footer;
